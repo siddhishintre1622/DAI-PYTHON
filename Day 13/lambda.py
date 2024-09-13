2c55a0177b2988c74c7f46f81055d83b2a43c71e
Siddhi Shintre <166989512+siddhishintre1622@users.noreply.github.com>
2024-09-13 23:20:16 +0530

    Add files via upload

---
+++ Day 13/lambda.py
@@ -0,0 +1,29 @@
+# cube = lambda x : x**3
+#
+# print(cube(3))
+#
+# str = "xyz"
+# str_len = lambda str : len(str)
+# print(str_len(str))
+#
+# iadd = lambda x,y : x+y
+# print(iadd(3,5))
+#
+'''
+#siddhi
+nums = list(input("Enter List : ").split())
+
+sq = list(map(lambda x : "positive" if int(x)>0 else "Negative" if int(x)<0 else 'zero'  ,nums))
+
+print(sq)
+
+'''
+
+
+
+'''
+numbers= [10,-5,0,7,-3,4,2]
+result = list(map(lambda x: "Positive" if x>0 else "Negative" if x<0 else "zero", numbers))
+print(result)
+'''
+
